3bd3d29df4081800dc7492337cd3953c1abc4f27
Omve <deepali.narkhede@corys.co.in>
2020-07-03 17:45:49 +0530

    Reporting , Creation of Simulator.

---
+++ DeepaliNarkhede/Training01/Training01/Trend/TrendLabelTransmit.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TrendLabel.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for TrendLabel.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Training01
+{
+    /// <summary>
+    /// Interaction logic for TrendLabel.xaml
+    /// </summary>
+    public partial class TrendLabelTransmit
+    {
+        public TrendLabelTransmit()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
