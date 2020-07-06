b653c7bb32b012aae5ae4151181f1208e7e08ae9
Omve <deepali.narkhede@corys.co.in>
2020-07-06 17:52:34 +0530

    Model & Graphics updation

---
+++ DeepaliNarkhede/Training01/Training01/Graphics/CS.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CS.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for CS.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TR01.Graphics
+{
+	using System.ComponentModel.Composition;
+	
+	/// <summary>
+	/// Interaction logic for CS.xaml
+	/// </summary>
+    [Export("CS")]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+	public partial class CS 
+	{
+		public CS()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
