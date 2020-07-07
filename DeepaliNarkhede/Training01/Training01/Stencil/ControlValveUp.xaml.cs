f10b812139a0d3ce8c343850370d60f4057616f4
Omve <deepali.narkhede@corys.co.in>
2020-07-07 17:50:29 +0530

    Progress Updated

---
+++ DeepaliNarkhede/Training01/Training01/Stencil/ControlValveUp.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ControlValveUp.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for ControlValveUp.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TR01.Stencil
+{
+	/// <summary>
+	/// Interaction logic for ControlValveUp.xaml
+	/// </summary>
+	public partial class ControlValveUp 
+	{
+		public ControlValveUp()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
---
+++ DeepaliNarkhede/Training01/Training01/Stencil/Transmitter01.xaml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Transmitter01.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for Transmitter01.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using RSI.Emulation.Controls.Control;
+
+namespace TR01.Stencil
+{
+	/// <summary>
+	/// Interaction logic for Transmitter01.xaml
+	/// </summary>
+	public partial class Transmitter01
+    {
+		public Transmitter01()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
