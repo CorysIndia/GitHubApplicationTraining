5ff11507c87ca9ec2aafc14da68b62a132ceea64
HYPERIONSYSTEMS\V.Wankhade <v.wankhadehyperionsystems.net>
2020-08-13 17:43:19 +0530

    I added FOD graphics:Vaibhav

---
+++ VaibhavWankhade/RSIStandardEmulationV1ToV3App1/RSIStandardEmulationV1ToV3App1/Graphics/Synoptic1.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Synoptic1.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for Synoptic1.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RSIStandardEmulationV1ToV3App1.Graphics
+{
+    using System.ComponentModel.Composition;
+
+    /// <summary>
+    /// Interaction logic for Synoptic1.xaml
+    /// </summary>
+    [Export("Synoptic1")]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class Synoptic1
+    {
+        public Synoptic1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
