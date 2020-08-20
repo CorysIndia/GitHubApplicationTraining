8e4114d4d34f17184d29990681605cbc763cfa75
AkankshaVJ <akanksha.jadhav@corys.co.in>
2020-08-20 13:30:24 +0530

    Button added to Homescreen

---
+++ Akanksha/RSIStandardEmulationApp1/RSIStandardEmulationApp1/ReportScreen.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReportScreen.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for ReportScreen.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RSIStandardEmulationApp1
+{
+    using System.ComponentModel.Composition;
+
+    /// <summary>
+    /// Interaction logic for ReportScreen.xaml
+    /// </summary>
+    [Export("ReportScreen")]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class ReportScreen
+    {
+        public ReportScreen()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Akanksha/RSIStandardEmulationApp1/RSIStandardEmulationApp1/Trend/Trend.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Trend.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for Trend.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RSIStandardEmulationApp1
+{
+    /// <summary>
+    /// Interaction logic for Trend.xaml
+    /// </summary>
+    public partial class Trend
+    {
+        public Trend()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
