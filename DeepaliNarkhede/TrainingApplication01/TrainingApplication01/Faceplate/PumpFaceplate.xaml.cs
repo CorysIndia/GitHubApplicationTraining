f062419d6212857e5979568bd74d8e8b471c7276
Omve <deepali.narkhede@corys.co.in>
2021-02-24 17:45:42 +0530

    Generation of Pump faceplate - Deepali.

---
+++ DeepaliNarkhede/TrainingApplication01/TrainingApplication01/Faceplate/PumpFaceplate.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PumpFaceplate.xaml.cs" company="RSI">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for PumpFaceplate.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TrainingApplication01.Faceplate
+{
+    using System.ComponentModel.Composition;
+
+    /// <summary>
+    /// Interaction logic for PumpFaceplate.xaml
+    /// </summary>
+    [Export("PumpFaceplate")]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class PumpFaceplate
+    {
+        public PumpFaceplate()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
